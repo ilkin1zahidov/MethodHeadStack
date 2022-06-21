f7574c880ece8fc33c5d4063703b2bcd6a2ffafe
ilkin1zahidov <ilkiniz@code.edu.az>
2022-06-22 01:39:59 +0400

    HeadStack

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodHeadStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(name1("Ilkin"));
+        }
+
+        static string name1(string name)
+        {
+            string result = "";
+            for (int i = name.Length - 1; i >= 0; i--)
+            {
+                result += name[i];
+            }
+            return result;
+        }
+    }
+    
+        
+}
